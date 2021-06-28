95259b2ad2daa9e7340f886bddffc08e98004a54
Hager Eldakroury <hagerdakroury@gmail.com>
2021-06-28 13:54:36 +0200

    Add commandline commands

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import { parseToJSON } from "./src/parser.js";
+import { program } from "commander";
 
-const fileExt = process.argv[2];
+program.option(
+  "-jt,--jsontree [edam-owl-file]",
+  "Generate a json representation of the EDAM hierarchy",
+  jsonTree
+);
 
-parseToJSON(fileExt);
+//to be implemented
+program.option(
+  "-jld,--jsonld [edam-owl-file]",
+  "Generate a json-ld formatted version of EDAM"
+);
+
+function jsonTree(fileExt, dummyPrevious) {
+  parseToJSON(fileExt);
+}
+
+program.parse();
