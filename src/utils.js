d77a16d98ef7e9ea786ba1e94c69d3ebc02bbc12
Hager Eldakroury <hagerdakroury@gmail.com>
2021-08-10 10:03:32 +0200

    feat: Add cli support

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,10 @@
-/*import * as fs from "fs";
+import * as fs from "fs";
 
 const writeJSONFile = (jsonObj) => {
   var file = fs.createWriteStream("tree.json");
-  file.on("error", function (err) {
-  });
+  file.on("error", function (err) {});
   file.write(JSON.stringify(jsonObj));
   file.end();
 };
 
 export { writeJSONFile };
-*/
